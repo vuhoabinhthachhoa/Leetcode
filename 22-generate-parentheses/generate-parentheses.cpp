0b07aee31574860ff167b07e01cb4840e014bd45
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2023-12-23 20:50:37 +0700

    Time: 9 ms (11.20%) | Memory: 16.8 MB (14.80%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void help(vector<string>& res, int n, string ds = "", int opens = 0, int closes = 0) {
+
+	if (opens == n && closes == n) {
+		res.push_back(ds);
+		return;
+	}
+
+	if (opens > closes && closes < n) {
+		ds.push_back(')');
+		help(res, n, ds, opens, closes + 1);
+		ds.pop_back();
+	}
+	
+	if (opens == n) return;
+	ds.push_back('(');
+	help(res, n, ds, opens + 1, closes);
+	ds.pop_back();
+}
+
+vector<string> generateParenthesis(int n) {
+	vector<string>res;
+	help(res, n);
+	return res;
+}
+};
