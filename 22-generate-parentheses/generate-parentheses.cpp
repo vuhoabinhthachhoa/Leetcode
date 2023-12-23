77253b8315c810d1bbbac7114b108741dd6d35e3
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2023-12-23 21:05:06 +0700

    Time: 9 ms (11.20%) | Memory: 15.2 MB (69.07%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     void help(vector<string>& res, int n, string ds = "", int opens = 0, int closes = 0) {
-
+	// if the number of opening and closing parethesis are equal to n, the ds is now a valid string
 	if (opens == n && closes == n) {
 		res.push_back(ds);
 		return;
 	}
 
+	// the number of closing parenthesis is always less than or equal to the numbers of opening one.
 	if (opens > closes) {
-		ds.push_back(')');
-		help(res, n, ds, opens, closes + 1);
-		ds.pop_back();
+		help(res, n, ds + ')', opens, closes + 1);
 	}
 	
+	// if the number of opening parenthesis is equal to n, return. his string cannot form a valid string.
+	// for example: ()( with n = 2. if we push (, the string would be ()(( --> invalid.
 	if (opens == n) return;
-	ds.push_back('(');
-	help(res, n, ds, opens + 1, closes);
-	ds.pop_back();
+	help(res, n, ds+ '(', opens + 1, closes);
 }
 
 vector<string> generateParenthesis(int n) {
