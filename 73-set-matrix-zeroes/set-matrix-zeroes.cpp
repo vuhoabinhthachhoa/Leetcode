02459576c999d8ab538fd0946c04f791a9dc92ae
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-04-27 12:37:16 +0700

    Time: 9 ms (56.97%) | Memory: 17.8 MB (10.14%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,21 +4,21 @@ public:
         int m = a.size();
         int n = a[0].size();
 
-        unordered_map<int, bool> rows;
-        unordered_map<int, bool> cols;
+        unordered_map<int, bool> rows_with_zeros;
+        unordered_map<int, bool> cols_with_zeros;
 
         for(int i = 0 ; i < m ; i++) {
             for(int j = 0 ; j < n ; j ++) {
                 if(a[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
+                    rows_with_zeros[i] = true;
+                    cols_with_zeros[j] = true;
                 }
             }
         }
 
         for(int i = 0 ; i < m ; i++) {
             for(int j = 0 ; j < n ; j ++) {
-                if(rows[i] == true || cols[j] == true) {
+                if(rows_with_zeros[i] == true || cols_with_zeros[j] == true) {
                     a[i][j] = 0;
                 }
             }
