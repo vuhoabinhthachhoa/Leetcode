6c547184282c6f3e981e6c4240ae5f9cd433ff99
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-04-29 10:35:35 +0700

    Time: 177 ms (12.92%) | Memory: 91.4 MB (54.80%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+   int minOperations(vector<int>& nums, int k) {
+    vector<int> oneCount(32, 0);
+    for (auto it : nums) {
+        for (int i = 0; i < 32; i++) {
+            // check i th bit of it
+            if ((it >> i) & 1) {
+                oneCount[i]++;
+            }
+        }
+    }
+
+    int res = 0;
+    for (int i = 0; i < 32; i++) {
+        // if i th bit of k is 1
+        if ((k >> i) & 1) {
+            // if number of 1 bit in i th bit in nums is even, we need to change it to odd
+            if (oneCount[i] % 2 == 0) {
+                res++;
+            }
+        }
+        // if i th bit of k is 0
+        else {
+            // if number of 1 bit in i th bit in nums is odd, we need to change it to even
+            if (oneCount[i] % 2 == 1) {
+                res++;
+            }
+        }
+    }
+
+    return res;
+}
+};
