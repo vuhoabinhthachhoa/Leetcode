1369818e2cf61070be0bc22e9f6b87f89010b061
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-05-03 08:45:52 +0700

    Time: 3 ms (39.03%) | Memory: 7.3 MB (93.86%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -4,15 +4,15 @@ public:
         int i = 0;
         int j = 0;
         while(i < version1.size() || j < version2.size() ) {
-            long long num1 = 0;
+            int num1 = 0;
             while(i < version1.size() && version1[i] != '.') {
-                num1 = num1*10 + version1[i] - '0';
+                num1 = num1*10 + (version1[i] - '0');
                 i++;
             }
 
-            long long num2 = 0;
+            int num2 = 0;
             while(j < version2.size() && version2[j] != '.') {
-                num2 = num2*10 + version2[j] - '0';
+                num2 = num2*10 + (version2[j] - '0');
                 j++;
             }
 
