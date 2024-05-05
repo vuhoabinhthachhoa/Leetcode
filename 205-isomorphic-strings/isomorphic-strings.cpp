317febe0d38a4d6fbda5d3cbfae472e208c87965
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-05-05 10:26:43 +0700

    Time: 8 ms (35.25%) | Memory: 9.4 MB (14.40%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> mp;
-        vector<bool> isUsed(128, false);
-        
-        for(int i = 0 ; i < s.size() ; i++) {
-            if(mp[s[i]] == '\0') {
-                if(isUsed[t[i]] == false){
-                    mp[s[i]] = t[i];
-                    isUsed[t[i]] = true;
-                }
-                else {
-                    return false;
-                }
-                
+   bool isIsomorphic(string s, string t) {
+    unordered_map<char, char> mp;
+    vector<bool> isUsed(128, false);
+    
+    for(int i = 0 ; i < s.size() ; i++) {
+        // If s[i] hasn't been mapped to any t[i] before.
+        if(mp[s[i]] == '\0') {
+            // If t[i] hasn't been used for mapping before.
+            if(isUsed[t[i]] == false) {
+                mp[s[i]] = t[i];
+                isUsed[t[i]] = true;
             }
+            // If t[i] has already been used for mapping, therefore we
+            // can't use t[i] for mapping to s[i], return false.
             else {
-                if(mp[s[i]] != t[i]) {
-                    return false;
-                }
+                return false;
             }
         }
-
-        return true;
+        // If s[i] has already been mapped to any t[i] before.
+        else {
+            // If s[i] does not map to t[i] as the same as the previous mapping, return false.
+            if(mp[s[i]] != t[i]) {
+                return false;
+            }
+        }
     }
+
+    return true;
+}
+
 };
