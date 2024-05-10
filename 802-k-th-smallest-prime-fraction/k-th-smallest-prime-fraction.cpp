18d55786b865e538095c3b4a605a7adc72de9fdd
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-05-10 11:19:26 +0700

    Time: 223 ms (69.63%) | Memory: 12.2 MB (81.43%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // Custom structure to store fraction and positions (i, j)
+// Custom structure to store fraction and positions (i, j)
 struct FractionWithPositions {
     pair<int, int> fraction;
     int i;
@@ -24,10 +24,22 @@ struct FractionWithPositionsCompare {
 };
 vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
     int n = arr.size();
+    // we have n - 1 row of fractions
+    // e.g : {1,2,3,7,11}
+    // 1/11 1/7 1/3 1/2
+    // 2/11 2/7 2/3
+    // 3/11 3/7
+    // 7/11
+    // In each row, the left fraction is always smaller than the right fraction
+    // In each column, the fraction above is always smaller that the fraction below
+    // for each step, we find the smallest fraction in the rest
+    // for example, after select 1/11 (0,0), we push the right and above fraction into the minHeap (to compare them), because the other 
+    // fractions are all greater than them.
+
     vector<int>selects(n-1);
     priority_queue<FractionWithPositions, vector<FractionWithPositions>, FractionWithPositionsCompare> minHeap;
     
-    // selects[i] is the number of fractions which were selected at row i
+    // selects[i] is the number of fractions which have already been selected at row i
 
     minHeap.push({ {arr[0], arr[n-1] }, 0, 0});
     while (true) {
@@ -42,10 +54,12 @@ vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         selects[i]++;
 
         // push the below fraction
+        // we can push below only if its left fraction has already been selected
         if (i < selects.size() - 1 && j == selects[i + 1]) {
             minHeap.push({{ arr[i + 1], arr[n - 1 - j] }, i + 1, j });
         }
         // push the right fraction
+        // we can push below only if its above fraction has already been selected
         if (i == 0 || j + 1 < selects[i - 1]) {
             minHeap.push({ { arr[i], arr[n - 1 - (j + 1)]}, i, j + 1});
         }
