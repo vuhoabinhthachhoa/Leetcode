806f439e8b7cf28ecd2dcc6f760bdc8e79842bd9
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-05-18 08:38:15 +0700

    Time: 4 ms (63.21%) | Memory: 15.7 MB (22.37%) - LeetSync

--- 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
@@ -12,15 +12,19 @@
 class Solution {
 public:
     int help(TreeNode* root, int& totalSteps) {
-    
         if(!root) {
             return 0;
         }
+        // left = 2 -> we need to to move 2 coins from currNode to left node
+        // left = -2 -> we need to to move 2 coins from left node to currNode 
+        // the same as right
         int left = help(root->left, totalSteps);
         int right = help(root->right, totalSteps);
 
         totalSteps += abs(left) + abs(right);
 
+        // returned value: if > 0 : it's the number of coins we need to move to the ancestor node
+        //  if < 0 : it's the number of coins we need to move from the ancestor node to currNode
         return left + right + root->val - 1;
     }
 
