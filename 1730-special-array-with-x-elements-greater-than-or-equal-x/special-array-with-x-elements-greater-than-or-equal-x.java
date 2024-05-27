6a670434e494e93b283ec969c83b0eeaf94fd8e8
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-05-27 08:23:47 +0700

    Time: 1 ms (78.32%) | Memory: 41.2 MB (38.48%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -5,10 +5,12 @@ class Solution {
         Arrays.sort(nums);
         int i = 0;
         while(i < nums.length) {
+            // validEles is the number of elements that are greater than or equal to the nums[i]
             int validEles = nums.length - i;
             if(validEles == nums[i]) {
                 return validEles;
             }
+            // if validEles is less than nums[i], that means we need to find the number that is equal to validEles and greater than the currNum
             if(validEles < nums[i]) {
                 if(validEles > currNum) {
                     return validEles;
@@ -17,6 +19,9 @@ class Solution {
                     return -1;
                 }
             }
+            // if validEles is greater than nums[i], that means we need to find the greater number than nums[i]
+            // for example: nums = {1,2,2,4,4,5}
+            // if nums[i] = 2, we have validEles = 5, so we need to find the number that is greater than 2
             currNum = nums[i];
             while(i < nums.length && nums[i] == currNum) {
                 i++;
@@ -24,4 +29,5 @@ class Solution {
         }
         return -1;
     }
+
 }
