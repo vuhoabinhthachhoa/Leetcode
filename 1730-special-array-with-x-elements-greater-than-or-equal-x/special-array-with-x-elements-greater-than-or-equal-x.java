a41a8d4b3a2d38ff61ac982c21b779ed388d8495
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-05-27 08:26:40 +0700

    Time: 0 ms (100.00%) | Memory: 41.3 MB (38.48%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,33 +1,25 @@
 class Solution {
     public int specialArray(int[] nums) {
-        int currNum = -1;
-        int res = 0;
-        Arrays.sort(nums);
-        int i = 0;
-        while(i < nums.length) {
-            // validEles is the number of elements that are greater than or equal to the nums[i]
-            int validEles = nums.length - i;
-            if(validEles == nums[i]) {
-                return validEles;
-            }
-            // if validEles is less than nums[i], that means we need to find the number that is equal to validEles and greater than the currNum
-            if(validEles < nums[i]) {
-                if(validEles > currNum) {
-                    return validEles;
-                }
-                else {
-                    return -1;
+        int l = 0;
+        int h = nums.length;
+        while(l <= h) {
+            int mid = l + (h - l) / 2;
+            int count = 0;
+            for(int i = 0; i < nums.length; i++) {
+                if(nums[i] >= mid) {
+                    count++;
                 }
             }
-            // if validEles is greater than nums[i], that means we need to find the greater number than nums[i]
-            // for example: nums = {1,2,2,4,4,5}
-            // if nums[i] = 2, we have validEles = 5, so we need to find the number that is greater than 2
-            currNum = nums[i];
-            while(i < nums.length && nums[i] == currNum) {
-                i++;
+            if(count > mid) {
+                l = mid + 1;
+            }
+            else if(count < mid) {
+                h = mid - 1;
+            }
+            else {
+                return mid;
             }
         }
         return -1;
     }
-
 }
