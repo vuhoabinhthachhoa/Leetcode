d6c1096ef74ae25255914973e8ab6062e64b9abd
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-06 16:03:31 +0700

    Time: 26 ms (79.33%) | Memory: 45.7 MB (27.26%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -7,6 +7,7 @@ class Solution {
         Map<Integer, Integer> mp = new HashMap<>();
 
         Arrays.sort(hand);
+        // count the frequency of each element
         for(int i = 0 ; i < n ; i++) {
             mp.put(hand[i], mp.getOrDefault(hand[i], 0) + 1);
         }
@@ -14,21 +15,19 @@ class Solution {
         for(int i = 0 ; i < n ; i++) {
             int val = mp.getOrDefault(hand[i], 0);
             mp.put(hand[i], 0);
-            if(val == 0) {
+            if (val == 0) {
                 continue;
             }
-
-            for(int j = 1 ; j < groupSize; j++) {
+            // for each element with frequency > 0, check if we can form a group of size groupSize starting from that element
+            // we sort the array so that we can assure that the element that we are currently checking is the smallest element in the group
+            for (int j = 1; j < groupSize; j++) {
                 int currVal = mp.getOrDefault((hand[i] + j), 0);
-                if(currVal >= val) {
+                if (currVal >= val) {
                     mp.put((hand[i] + j), currVal - val);
-                }
-                else return false;
+                } else return false;
             }
         }
-        // 1 2 2 3 3 4 6 7 8
-
-
+        
         return true;
     }
 }
