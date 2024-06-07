f3561aeb2526798306ece7e3f1e0b391d2101788
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-07 09:15:00 +0700

    Time: 375 ms (15.64%) | Memory: 64.7 MB (10.34%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,13 +1,57 @@
 class Solution {
-     public String replaceWords(List<String> dictionary, String sentence) {
-        Set<String> set = new HashSet<>(dictionary);
+     class TrieNode {
+        TrieNode[] childNode;
+        boolean wordEnd;
+
+        TrieNode () {
+            childNode = new TrieNode[26];
+            wordEnd = false;
+        }
+    }
+
+    class Trie {
+        TrieNode root;
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(String key) {
+            TrieNode curr = root;
+            for(int i = 0 ; i < key.length() ; i++) {
+                int ind = key.charAt(i) - 'a';
+                if(curr.childNode[ind] == null) {
+                    curr.childNode[ind] = new TrieNode();
+                }
+                curr =  curr.childNode[ind];
+            }
+            curr.wordEnd = true;
+        }
+
+        boolean search(String key) {
+            TrieNode curr = root;
+            for(int i = 0 ; i < key.length() ; i++) {
+                int ind = key.charAt(i) - 'a';
+                if(curr.childNode[ind] == null) {
+                    return false;
+                }
+                curr =  curr.childNode[ind];
+            }
+            return curr.wordEnd;
+        }
+
+    }
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Trie trie = new Trie();
+        for(String word : dictionary) {
+            trie.insert(word);
+        }
 
         String[] words = sentence.split(" ");
         StringBuilder res = new StringBuilder();
 
         for(String word : words) {
             StringBuilder root = new StringBuilder();
-            for(int i = 0; i < word.length()&&!set.contains(root.toString());i++) {
+            for(int i = 0; i < word.length()&&!trie.search(root.toString());i++) {
                 root.append(word.charAt(i));
             }
             res.append(root);
@@ -17,5 +61,6 @@ class Solution {
         res.deleteCharAt(res.length()-1);
         return res.toString();
     }
+
 
 }
