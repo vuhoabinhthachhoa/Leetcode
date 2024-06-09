66c4f2a5d9d86d938caacd369486a169f56fdc03
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-09 08:38:22 +0700

    Time: 32 ms (5.73%) | Memory: 48 MB (37.01%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,5 +1,27 @@
 class Solution {
-        public int subarraysDivByK(int[] nums, int k) {
+       /*
+     * prefixSum[i] - prefixSum[j] is the sum of elements between index i and j
+     * => prefixSum[i] - prefixSum[j] = n * k
+     * => (prefixSum[i] - prefixSum[j]) % k = 0
+     * We have:
+     * prefixSum[i] = Q1 * k + R1
+     * prefixSum[j] = Q2 * k + R2
+     * => (prefixSum[i] - prefixSum[j]) % k = (Q1 * k + R1 - Q2 * k - R2) % k = 0
+     * => ((Q1 - Q2) * k + (R1 - R2)) % k = 0
+     * => (R1 - R2) is also divisible by k, another thing is that R1 and R2 are in the range of 0 to k - 1
+     * (because R1 and R2 are the remainder when dividing by k)
+     * => R1 - R2 is divisible by k if
+     *  + (R1 - R2) = 0 => R1 = R2
+     *  + (R1 - R2) = k => R1 = R2 + k
+     *  + (R1 - R2) = -k => R1 = R2 - k
+     *  How to calculate the number of valid subarrays?
+     *  when we meet prefixSum[i], we need to know how many prefixSum[j] (j < i) that satisfy the condition (count)
+     *  +) prefixSum[j] % k = prefixSum[i] % k
+     *  +) prefixSum[j] % k = prefixSum[i] % k + k
+     *  +) prefixSum[j] % k = prefixSum[i] % k - k
+     *  => We have {count} subarrays more
+     */
+    public int subarraysDivByK(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0,  1);
         int prefixSum = 0;
