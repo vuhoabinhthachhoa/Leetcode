a8caecd4c4b4ca9733dbeec47e16e0be9c878119
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-15 09:58:34 +0700

    Time: 82 ms (72.14%) | Memory: 63.2 MB (19.91%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,21 +1,22 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
         int n = profits.length;
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for(int i = 0; i < n ; i++) {
-            maxHeap.add(new int[]{profits[i], capital[i]});
+        int[][] projects = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            projects[i] = new int[]{profits[i], capital[i]};
         }
-        while(k-- > 0 && !maxHeap.isEmpty()) {
-            List<int[]> temp = new ArrayList<>();
-            while(!maxHeap.isEmpty() && maxHeap.peek()[1] > w) {
-                temp.add(maxHeap.poll());
+        Arrays.sort(projects, (a, b) -> a[1] - b[1]);
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        int i = 0;
+        while(k-- > 0 ) {
+            while(i < n && projects[i][1] <= w) {
+                maxHeap.offer(projects[i++][0]);
             }
-            int[] currProject = maxHeap.poll();
-             if(currProject == null) {
+            if(maxHeap.isEmpty()) {
                 break;
             }
-            w += currProject[0];
-            maxHeap.addAll(temp);
+            w += maxHeap.poll();
         }
         return w;
     }
