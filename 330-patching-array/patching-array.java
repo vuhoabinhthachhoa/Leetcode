38adbf9b94d366744cf61c53ca7a032c3c403cea
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-16 10:40:23 +0700

    Time: 0 ms (100.00%) | Memory: 43.8 MB (78.11%) - LeetSync

--- 330-patching-array/patching-array.java
+++ 330-patching-array/patching-array.java
@@ -1,4 +1,17 @@
 class Solution {
+     /*
+    * Intuition:
+    * range indicates that we can build up the sum from 1 to range.
+    * First, we initialize range as 0
+    * We iterate the nums array:
+    * - The next sum we need to build is range + 1
+    * - If nums[i] > range + 1, that means there is no way to build the sum (range + 1) by using the current elements in the array 
+    *   (because all the remaining elements in the arrays are greater than (range +1))
+    *   + So we need to add (range + 1) to the array (it's one patch), so increase the res by 1
+    * - If nums[i] <= range + 1, so we can use the nums[i] (that means we can add nums[i] to the current range) 
+    *   +) for example: the current range is 5 and we meet nums[i] = 3, so we can use nums[i] to build up the sum
+    *      and the range now is 5 + 3 = 8 because we can build sum(6) by using 3 and 3, sum(7) by using 3 and 4, sum(8) by using 3 and 5
+    * */
     public int minPatches(int[] nums, int n) {
         int res = 0;
         long range = 0;
@@ -13,7 +26,10 @@ class Solution {
                 i++;
             }
         }
-
+        // after we've used all the elements in the arrays, but the range is still less than n
+        // we need to continue adding the elements to the array
+        // for each element we add, the range will be increased by (range + 1)
+        // loop unitl the range is equal to or greater than n
         while(range < n) {
             range += range + 1;
             res ++;
