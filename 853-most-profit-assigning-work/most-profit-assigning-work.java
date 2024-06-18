6f9527d964fd2f59500bde6395a4026f714f8fd1
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-18 07:35:51 +0700

    Time: 32 ms (30.22%) | Memory: 45.4 MB (78.50%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int[][] job = new int[profit.length][2];
+        for(int i = 0 ; i < profit.length ; i++) {
+            job[i][0] = difficulty[i];
+            job[i][1] = profit[i];
+        }
+        Arrays.sort(job, Comparator.comparing(a -> a[0]));
+        Arrays.sort(worker);
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        int maxProfit = 0;
+        int j = 0;
+        for(int i = 0 ; i < worker.length ; i++) {
+            while(j < job.length && worker[i] >= job[j][0]) {
+                maxHeap.add(job[j][1]);
+                j++;
+            }
+            if(maxHeap.isEmpty()) continue;
+            maxProfit += maxHeap.peek();
+        }
+        return maxProfit;
+    }
+}
