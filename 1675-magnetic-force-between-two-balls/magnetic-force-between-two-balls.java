7f17812ac16932125f852ede08b54942c8d524f8
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-20 09:42:36 +0700

    Time: 36 ms (97.32%) | Memory: 55.5 MB (78.33%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,16 +1,20 @@
 class Solution {
    boolean isValidForce(int[] position, int m, int distance) {
+        // we always put the first ball at the first position
         int currPos = position[0];
-        m--;
+        m--; // we have already put one ball, so we need to put remaining m-1 balls
         int i = 1;
         while(i < position.length) {
+            // for each ball, we find the next position that is at least [distance] away from the current ball
             if(position[i] - currPos >= distance) {
                 currPos = position[i];
                 m--;
+                // if we have put all the balls, we return true
                 if(m == 0) return true;
             }
             i++;
         }
+        // if we have not put all the balls, we return false
         return false;
     }
     public int maxDistance(int[] position, int m) {
@@ -28,5 +32,4 @@ class Solution {
         }
         return high;
     }
-
 }
