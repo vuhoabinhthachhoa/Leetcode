6bd8cdb087be315663962f5c3b7fd44a4c48b11a
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-06-22 08:41:49 +0700

    Time: 7 ms (96.52%) | Memory: 54.9 MB (45.80%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,17 +1,34 @@
 class Solution {
-     public int numberOfSubarrays(int[] nums, int k) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        int res = 0;
+      public int numberOfSubarrays(int[] nums, int k) {
+        int left = 0, right = 0;
         int odds = 0;
-        mp.put(0,1);
-        for(int i =0 ; i < nums.length ;i++) {
-            if(nums[i] % 2 == 1) {
-                odds++;
-            }
-            int freq = mp.getOrDefault(odds - k, 0);
-            res += freq;
-            mp.put(odds, mp.getOrDefault(odds,0) +1);
+        int res = 0;
+        while(right < nums.length && odds < k) {
+            if(nums[right] % 2 == 1) odds++;
+            right++;
         }
+        if(odds < k) return 0;
+        
+        int leftEvens = 0;
+        int rightEvens = 0;
+        while(right < nums.length) {
+            if(nums[right] % 2 == 0) rightEvens++;
+            else {
+                while(nums[left] % 2 == 0) {
+                    left++;
+                    leftEvens++;
+                }
+                left++;
+                res += (leftEvens + 1)  * (rightEvens + 1);
+                leftEvens = rightEvens = 0;
+            }
+            right++;
+        }
+        while(nums[left] % 2 == 0) {
+            left++;
+            leftEvens++;
+        }
+        res += (leftEvens + 1)  * (rightEvens + 1);
         return res;
     }
 }
