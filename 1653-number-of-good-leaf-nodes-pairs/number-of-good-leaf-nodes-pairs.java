b15c83cb8c908f95cfb8e2ec4255e3823b4492fb
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-07-18 11:46:41 +0700

    Time: 2 ms (95.76%) | Memory: 45.1 MB (20.00%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
@@ -14,19 +14,23 @@
  * }
  */
 class Solution {
+    // mp is used as a map to store the number of leaf nodes at each distance
+    // the return value is the number of valid pairs
     int help(TreeNode root, int[] mp, int distance) {
         if(root == null) {
             return 0;
         }
+        // if it is a leaf node, we set mp[0] to 1 like now there is a leaf node at distance 0
         if(root.left == null && root.right == null) {
             mp[0] = 1;
             return 0;
         }
         int left = help(root.left, mp, distance);
-        // copy mp to leftLeafs
-        int[] leftLeafs = new int[distance];
+
+        // copy mp to leftMp
+        int[] leftMp = new int[distance];
         for(int i = 0 ; i < distance ; i++) {
-            leftLeafs[i] = mp[i];
+            leftMp[i] = mp[i];
         }
         // clear mp
         for(int i = 0 ; i < distance ; i++) {
@@ -34,27 +38,38 @@ class Solution {
         }
 
         int right = help(root.right, mp, distance);
-        int[] rightLeafs = new int[distance];
+        // copy mp to rightMp
+        int[] rightMp = new int[distance];
         for(int i = 0 ; i < distance ; i++) {
-            rightLeafs[i] = mp[i];
+            rightMp[i] = mp[i];
         }
 
+        // calculate the mp for the current node
+        // mp[i] means the number of leaf nodes at distance i
+        // as we the current node is one step further from the leaf nodes compared to its children nodes, we need to add 1 to the distance
+        // for example if leftMp[0] = 1 and rightMp[0] = 1, then mp[1] = 1 + 1 = 2
+        // because the current node is not a leaf node, so mp[0] always be 0
         mp[0] = 0;
         for(int i = 1 ; i < distance ; i++) {
-            mp[i] = leftLeafs[i-1] + rightLeafs[i-1];
+            mp[i] = leftMp[i-1] + rightMp[i-1];
         }
 
+        // match all the valid pairs
         int currPairs = 0;
-
-        // prefix sum rightLeafs
+        // do prefix sum on rightMp to calculate the number of leaf nodes that are at distance i or less than i on the right subtree
         for(int i = 1 ; i < distance ; i++) {
-            rightLeafs[i] += rightLeafs[i-1];
+            rightMp[i] += rightMp[i-1];
         }
-
+        // for each distance i on the left subtree, we need to find the number of leaf nodes on the right subtree
+        // such that the sum of the distance of each pair is less than or equal to distance
+        // from left subtree move up 1 step, then move down another 1 step to the right subtree, then
+        // the sum of the distance is right distance + left distance + 2
+        // therefore, right distance = distance - 2 - left distance
         for(int i = 0 ; i <= distance-2 ; i++) {
-            currPairs += leftLeafs[i] * rightLeafs[distance - 2 - i];
+            currPairs += leftMp[i] * rightMp[distance - 2 - i];
         }
 
+        // combine all the valid pairs from the left subtree, right subtree and the current node itself and return
         return left + right + currPairs;
     }
 
@@ -62,4 +77,5 @@ class Solution {
         int[] mp = new int [distance];
         return help(root, mp, distance);
     }
+
 }
