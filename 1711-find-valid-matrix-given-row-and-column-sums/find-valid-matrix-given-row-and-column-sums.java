d01319b3873c75fc7c047e6c43fec126f6189e8b
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-07-20 21:40:57 +0700

    Time: 7 ms (52.59%) | Memory: 53.4 MB (6.77%) - LeetSync

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int m = rowSum.length;
+        int n = colSum.length;
+        int[][] res = new int[m][n];
+        long currColSum = 0;
+        for(int i = 0 ; i < m; i ++) {
+            res[i][0] = rowSum[i];
+            currColSum += rowSum[i];
+        }
+
+        for(int i = 0 ; i < n - 1; i++) {
+            long excess = currColSum - colSum[i];
+            currColSum = 0;
+            for(int j = 0 ; j < m; j++) {
+                if(res[j][i] <= excess) {
+                    excess -= res[j][i];
+                    res[j][i + 1] = res[j][i];
+                    res[j][i] = 0;
+                    currColSum += res[j][i + 1];
+                }
+                else {
+                    res[j][i] -= excess;
+                    res[j][i + 1] = (int)excess;
+                    currColSum += excess;
+                    break;
+                }
+            }
+        }
+
+
+
+        return res;
+    }
+}
