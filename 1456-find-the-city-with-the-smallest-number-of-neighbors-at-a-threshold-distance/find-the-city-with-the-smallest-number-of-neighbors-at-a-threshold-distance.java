09881757f506761a7dc69dbd33fc4c1a91d0132d
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-07-28 14:17:35 +0700

    Time: 99 ms (6.57%) | Memory: 44.3 MB (56.19%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,50 +1,94 @@
-class Solution {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        // first convert graph to adjacency list representation
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] edge : edges) {
-            int node1 = edge[0], node2 = edge[1], distance = edge[2];
-            graph.get(node1).add(new int[]{node2, distance});
-            graph.get(node2).add(new int[]{node1, distance});
-        }
+class CityDistancePair {
+    int currentCity;
+    int currentDistance;
 
-        int minimum_number = n;
-        int res = -1;
-
-        for (int source = 0; source < n; source++) {
-            int neighbors = get_number_of_neighbors_in_distance(graph, source, n, distanceThreshold);
-            // we iterate source from smaller to bigger this ensures that we choose node with greater value if they have equal number of neighbors
-            if (neighbors <= minimum_number) {
-                minimum_number = neighbors;
-                res = source;
-            }
-        }
-
-        return res;
+    public CityDistancePair(int currentCity, int currentDistance) {
+        this.currentCity = currentCity;
+        this.currentDistance = currentDistance;
     }
 
-    private int get_number_of_neighbors_in_distance(List<List<int[]>> graph, int source, int n, int distanceThreshold) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        minHeap.add(new int[]{0, source}); // distance to node itself is 0
-        Set<Integer> visited = new HashSet<>();
+    public int getCurrentCity() {
+        return currentCity;
+    }
 
-        while (!minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int distance_to_this_node = top[0], cur_node = top[1];
-            if (!visited.contains(cur_node)) {
-                visited.add(cur_node);
-                for (int[] neighbor : graph.get(cur_node)) {
-                    int distance_from_source = distance_to_this_node + neighbor[1];
-                    if (distance_from_source <= distanceThreshold) { // ensure that we're allowed to go to this node
-                        minHeap.add(new int[]{distance_from_source, neighbor[0]});
+    public int getCurrentDistance() {
+        return currentDistance;
+    }
+}
+
+
+class Solution {
+        public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        ArrayList<Integer>[] adj = new ArrayList[n];
+        for(int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+
+        // build adjacent list
+        for(int i = 0 ; i < edges.length ; i++) {
+            int src = edges[i][0];
+            int des = edges[i][1];
+            int length = edges[i][2];
+            adj[src].add(des);
+            adj[src].add(length);
+            adj[des].add(src);
+            adj[des].add(length);
+        }
+
+        int[][] dist = new int[n][n];
+        PriorityQueue<CityDistancePair> minHeap = new PriorityQueue<>(new Comparator<CityDistancePair>() {
+            @Override
+            public int compare(CityDistancePair pair1, CityDistancePair pair2) {
+                return pair1.getCurrentDistance() - pair2.getCurrentDistance();
+            }
+        });
+
+        boolean[] visited = new boolean[n];
+        minHeap.add(new CityDistancePair(0, 0));
+        while(!minHeap.isEmpty()){
+            CityDistancePair curr = minHeap.poll();
+            int currCity = curr.getCurrentCity();
+            int currDist = curr.getCurrentDistance();
+            visited[currCity] = true;
+            for(int i = 0 ; i < adj[currCity].size() ; i+=2) {
+                int nextCity = adj[currCity].get(i);
+
+                int length = adj[currCity].get(i + 1);
+                for(int j = 0 ; j < n ; j++) {
+                    if(dist[currCity][j] != 0 && j != nextCity) {
+                        int newDist = dist[currCity][j] + length;
+                        if(dist[nextCity][j] == 0 || dist[nextCity][j] > newDist) {
+                            dist[nextCity][j] = newDist;
+                            dist[j][nextCity] = newDist;
+                        }
                     }
                 }
+                if(dist[currCity][nextCity] == 0 || dist[currCity][nextCity] > length) {
+                    dist[currCity][nextCity] = length;
+                    dist[nextCity][currCity] = length;
+                }
+                if(visited[nextCity]) {
+                    continue;
+                }
+                minHeap.add(new CityDistancePair(nextCity, currDist + length));
             }
         }
-        // actually you can return visited.size() and with math there will be nothing wrong but actually we have visited.size() - 1 neighbors since we're not neighbor of ourselves
-        return visited.size() - 1;
+
+        int min = n-1;
+        int res = -1;
+        for(int i = 0 ; i < n ; i++) {
+            int currDistanceThreshold = 0;
+            for(int j = 0 ; j < n ; j++) {
+                if(dist[i][j] <= distanceThreshold && dist[i][j] != 0) {
+                    currDistanceThreshold ++;
+                }
+            }
+            if(currDistanceThreshold <= min) {
+                min = currDistanceThreshold;
+                res = i;
+            }
+        }
+        return res;
     }
+ 
 }
