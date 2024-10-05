53ebf299ad8db745a3113c4db7a430c40db8885d
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-10-05 17:37:05 +0700

    Time: 22 ms (42.51%) | Memory: 44.3 MB (32.49%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -9,9 +9,16 @@ class Solution {
         // do sliding window
         int left = 0;
         for(int right = 0 ; right < s2.length() ; right++) {
+            // remain = the frequent of s2[right] in s1 - the frequent of s2[right] in the substring that we are considering in s2
+            // for example: s1: xxyy ; s2: xyabcd
+            // if the substring is xy: the remain of x would be 1 and y would be 1
             int remain = mp.getOrDefault(s2.charAt(right), 0);
             if(remain == 0) {
-                while(left < right && s2.charAt(left) != s2.charAt(right)) {
+                // remain = 0 means that we can't add s2[right] into the substring
+                // so, we need to shrink the substring by increasing left until we meet the s2[right].
+                while(s2.charAt(left) != s2.charAt(right)) {
+                    // if the character that we are removing from the substring is in s1, 
+                    // we have to increase its remain by 1
                     if (s1.contains(Character.toString(s2.charAt(left)))) {
                         remain = mp.getOrDefault(s2.charAt(left), 0);
                         mp.put(s2.charAt(left), remain+1);
@@ -29,5 +36,5 @@ class Solution {
         }
         return false;
     }
-    
+       
 }
