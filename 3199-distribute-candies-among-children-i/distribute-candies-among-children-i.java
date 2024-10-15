b2c276e5e2416fffc87c8b37e64c2b8f4c609255
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-10-15 19:59:02 +0700

    Time: 1 ms (98.09%) | Memory: 41.3 MB (6.69%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.java
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.java
@@ -1,23 +1,36 @@
 class Solution {
-    public int distributeCandies(int n, int limit) {
+     public int distributeCandies(int n, int limit) {
+        // min is the minimum number of candies that can be distributed for each child
+        // each child can receive from min to limit candies
+        // Math.max to avoid that n - 2*limit < 0
         int min = Math.max(0, n - 2*limit);
+        
         int[] prevDp = new int[n + 1];
         int[] currDp = new int[n + 1];
+        // do Math.min to avoid case of limit > n
         limit = Math.min(limit, n);
+        
         // initialize value
         for(int i = min ; i <= limit ; i++) {
+            // prev[i] is the number of ways to distribute i candies to the first child
+            // for the first child, there is always only one way to distribute (min -> limit) candies
             prevDp[i] = 1;
         }
         for(int i = 0 ; i < n + 1 ; i++) {
-            int val = 0;
+            int total = 0;
+            // j is the number of candies that the second child receives
+            // so the number of ways to distribute i candies to the first two children is
+            // the sum of the number of ways to distribute i-j candies to the first child
             for(int j = min ; j <= limit ; j++ ) {
                 if(i - j >= 0) {
-                    val += prevDp[i - j];
+                    total += prevDp[i - j];
                 }
             }
-            currDp[i] = val;
+            currDp[i] = total;
         }
         int res = 0;
+        // the number of ways to distribute n candies to the first child is the sum of the number 
+        // of ways to distribute n-i candies to the first two children
         for(int i = min ; i <= limit ; i++ ) {
             res += currDp[n - i];
         }
