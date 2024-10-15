bc17a0f2e8dcaf53455146b44f67cedf7bfb96a7
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-10-15 13:56:51 +0700

    Time: 7 ms (100.00%) | Memory: 45.4 MB (41.94%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -3,6 +3,10 @@ class Solution {
         int whiteCount = 0;
         long res = 0;
         for(int i = 0 ; i < s.length() ; i++) {
+            // whiteCount is the number of white cells before the current cell, that also means the current white cells is (whiteCount)th white cell
+            // (the first white cell is 0th)
+            // if the current white cell is at i, and it is the (whiteCount)th white
+            // that means we need to move the current cell from the position i to the position whiteCount, it will take i - whiteCount steps
             if(s.charAt(i) == '0') {
                 res += (i - whiteCount);
                 whiteCount++;
