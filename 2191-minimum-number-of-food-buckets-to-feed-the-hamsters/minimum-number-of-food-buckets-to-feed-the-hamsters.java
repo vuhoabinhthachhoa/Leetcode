a27fb24a5f8dd72880907f9f2a9a18df9848a238
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2024-11-01 09:34:03 +0700

    Time: 5 ms (51.11%) | Memory: 45.3 MB (23.16%) - LeetSync

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.java
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.java
@@ -6,19 +6,25 @@ class Solution {
         int i = 0;
         while(i < n) {
             if(hamsters.charAt(i) == 'H') {
+                // if we already added a bucket on the left, current hamster is fed
                 if(rightMostBucket == i-1 ) {
                     i++;
                     continue;
                 }
+                // else we have to add a new bucket for current hamster
+                // we prioritize to add the new bucket to the right, because by doing this the new bucket might feed 2 hamsters, which allows us 
+                // to minimize the number of buckets
                 if(i < n - 1 && hamsters.charAt(i+1) == '.') {
                     res++;
                     rightMostBucket = i+1;
                 }
+                // if we can't add to the right, we have to add to the left
                 else {
                     if(i >= 1 && hamsters.charAt(i-1)=='.'){
                         res++;
                         rightMostBucket = i-1;
                     }
+                    // if we even can't add to the left, which means it's impossible to feed the current hamster
                     else {
                         return -1;
                     }
@@ -28,6 +34,5 @@ class Solution {
 
         }
         return res;
-    }
-
+}
 }
