85c66c48ff46ceec3205e826c059aa6ffc8fc3e8
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2025-02-17 12:22:49 +0700

    Time: 1 ms (98.69%) | Memory: 42.8 MB (22.71%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -1,28 +1,31 @@
 class Solution {
-     public static int[] shortestToChar(String s, char c) {
+    public static int[] shortestToChar(String s, char c) {
         int n = s.length();
         int[] ans = new int[n];
-        Arrays.fill(ans, n);
+
+        int rightMostPos = -n;
+        // traverse from left to right
         for(int i = 0 ; i < n ; i++) {
             if(s.charAt(i) == c) {
                 ans[i] = 0;
-                int left = i - 1;
-                while(left >= 0 && s.charAt(left) != c) {
-                    if(i - left < ans[left]) {
-                        ans[left] = i - left;
-                    }
-                    left--;
-                }
-
-                int right = i + 1;
-                while(right < n && s.charAt(right) != c) {
-                    if(right - i < ans[right]) {
-                        ans[right] = right - i;
-                    }
-                    right++;
-                }
+                rightMostPos = i;
+            }
+            else {
+                ans[i] = i - rightMostPos;
             }
         }
+
+        int leftMostPos = 2*n;
+        // traverse from right to left
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            if(s.charAt(i) == c) {
+                leftMostPos = i;
+            }
+            else {
+                ans[i] = Integer.min(ans[i], leftMostPos - i);
+            }
+        }
+
 
         return ans;
     }
