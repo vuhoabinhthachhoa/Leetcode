1dee91538c4e2ac8b6e58c470b1202ba362251b9
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2025-02-18 12:03:09 +0700

    Time: 1 ms (91.27%) | Memory: 43.1 MB (18.84%) - LeetSync

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int left = 0;
+        int res = 1;
+        // exceptional cases
+        if(nums.length == 1) {
+            return 1;
+        }
+        if(nums.length == 2) {
+            if(nums[0] == nums[1]) {
+                return 1;
+            }
+            return 2;
+        }
+        for(int right = 1 ; right < nums.length - 1 ; right ++) {
+            if(nums[right] == nums[right - 1]) {
+                left = right;
+            }
+            res = Math.max(res, right - left + 1);
+            if(nums[right] < nums[right - 1] && nums[right] < nums[right + 1] || nums[right] > nums[right - 1] && nums[right] > nums[right + 1]) {
+                left = right;
+            }
+        }
+        if(nums[nums.length - 1] != nums[nums.length - 2]) {
+            res = Math.max(res, (nums.length - 1) - left + 1);
+
+        }
+        return res;
+    }
+}
