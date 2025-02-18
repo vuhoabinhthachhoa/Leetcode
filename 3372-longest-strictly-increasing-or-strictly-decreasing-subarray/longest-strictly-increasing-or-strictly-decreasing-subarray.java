808bd48282fa9bf074b40b07a3eb7d793848b71b
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2025-02-18 12:05:28 +0700

    Time: 1 ms (91.27%) | Memory: 43.3 MB (6.92%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,11 +1,12 @@
 class Solution {
-    public int longestMonotonicSubarray(int[] nums) {
+     public int longestMonotonicSubarray(int[] nums) {
         int left = 0;
         int res = 1;
         // exceptional cases
         if(nums.length == 1) {
             return 1;
         }
+
         if(nums.length == 2) {
             if(nums[0] == nums[1]) {
                 return 1;
@@ -13,9 +14,11 @@ class Solution {
             return 2;
         }
         for(int right = 1 ; right < nums.length - 1 ; right ++) {
+            // we can't include the current num in this case
             if(nums[right] == nums[right - 1]) {
                 left = right;
             }
+            // we update the res before checking because the current num is also in our current subarray
             res = Math.max(res, right - left + 1);
             if(nums[right] < nums[right - 1] && nums[right] < nums[right + 1] || nums[right] > nums[right - 1] && nums[right] > nums[right + 1]) {
                 left = right;
