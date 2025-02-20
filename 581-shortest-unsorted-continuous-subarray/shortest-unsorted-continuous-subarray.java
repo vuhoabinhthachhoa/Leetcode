fa3d5edcc308463600b834817053ffff1ffbb520
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2025-02-20 09:56:14 +0700

    Time: 1 ms (95.98%) | Memory: 45.6 MB (31.18%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -1,8 +1,10 @@
 class Solution {
-    public int findUnsortedSubarray(int[] nums) {
+   public int findUnsortedSubarray(int[] nums) {
         int max = Integer.MIN_VALUE;
         int i = 0;
+        // right is the position of the right most element that is at wrong position
         int right = 0;
+        // minWrongEle is the minimum element of the wrong position elements
         int minWrongEle = Integer.MAX_VALUE;
         while(i < nums.length) {
             if(nums[i] < max) {
@@ -18,6 +20,7 @@ class Solution {
         while(left < nums.length && nums[left] <= minWrongEle) {
             left++;
         }
+        // to have to sort from the element that is greater than the minWrongEle.
         return (right - left + 1) > 1 ? (right - left + 1) : 0;
     }
 }
