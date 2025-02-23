812b48abd36796e0ab5a651e42fe9d24b4d946b7
html <148322937+vuhoabinhthachhoa@users.noreply.github.com>
2025-02-23 16:48:24 +0700

    Time: 1 ms (53.87%) | Memory: 43.6 MB (32.59%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -15,14 +15,20 @@
  */
 class Solution {
     Map<Integer, Integer> map = new HashMap<>();
+    // leftEdge is the right most of the elements which have already been built
     int leftEdge = 0;
+    // ind is the index of current element (in preorder array)
     int ind = 0;
 
     public TreeNode buildTree(int[] preorder, int[] postorder) {
         if(ind == preorder.length) return null;
         int curVal = preorder[ind];
         TreeNode root = new TreeNode(curVal);
+        // move to the next element
         ind++;
+        // if current element's index equals to leftEdge, that means all of its children have already been built
+        // So we stop here
+        // we have to repeat this action after building left tree and right tree as well
         if(map.get(curVal) == leftEdge) {
             leftEdge++;
             return root;
@@ -47,3 +53,4 @@ class Solution {
         return buildTree(preorder, postorder);
     }
 }
+
